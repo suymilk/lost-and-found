17de5345dde3e1c56db7c8f122d5360ee1653175
Suyeon Son <suyeon.son@icloud.com>
2014-09-17 16:02:47 -0500

    clean up, comment

--- xmltojson.py
+++ xmltojson.py
@@ -1,23 +1,24 @@
-# from xml.dom import minidom
-import xml.etree.ElementTree as ET
-import urllib
 import json
+import urllib
+import xml.etree.ElementTree as ET
 
 url = "http://advisory.mtanyct.info/LPUWebServices/CurrentLostProperty.aspx"
-url_content = urllib.urlopen(url).read()
+url_content = urllib.urlopen(url).read() # read content from the url 
+root = ET.fromstring(url_content) # get root node
 
-root = ET.fromstring(url_content)
+categories = [] # list of categories of lost/found items
+d = {} # dictionary of categories, subcategories and their counts
 
-categories = []
-d = {}
-
+# for each category of items, create a dictionary with the category as the keys and set the category's children nodes (item, count of item) as their values
 for children in root:
 	if children.tag == "Category":
 		for child in children:
 			d[children.attrib.get(children.tag)] = [child.attrib]
 
+# json-fy everything
 json_input = json.dumps(d, sort_keys = True, indent = 4, separators = (',', ": "))
 
+# print json-fied content to an actual json file
 json_output = open("lostandfound.json", "w")
 print >>json_output, json_input
 
