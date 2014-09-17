219372427c87a4b48a298e48c9af4365d170764a
Suyeon Son <suyeon.son@icloud.com>
2014-09-17 15:52:52 -0500

    actual json formatting w json file output

--- xmltojson.py
+++ xmltojson.py
@@ -1,6 +1,7 @@
 # from xml.dom import minidom
 import xml.etree.ElementTree as ET
 import urllib
+import json
 
 url = "http://advisory.mtanyct.info/LPUWebServices/CurrentLostProperty.aspx"
 url_content = urllib.urlopen(url).read()
@@ -13,12 +14,10 @@ d = {}
 for children in root:
 	if children.tag == "Category":
 		for child in children:
-			d[children.attrib.get(children.tag)] = child.attrib
+			d[children.attrib.get(children.tag)] = [child.attrib]
 
+json_input = json.dumps(d, sort_keys = True, indent = 4, separators = (',', ": "))
 
-print d
-
-
-# json = open("lostandfound.json", "w")
-# print >>json, e
+json_output = open("lostandfound.json", "w")
+print >>json_output, json_input
 
