215f84ae2217922067511d34fb004800f8c3cdb3
Suyeon Son <suyeon.son@icloud.com>
2014-09-18 14:58:17 -0500

    reformat json to work w d3

--- xmltojson.py
+++ xmltojson.py
@@ -7,20 +7,25 @@ url_content = urllib.urlopen(url).read() # read content from the url
 root = ET.fromstring(url_content) # get root node
 
 categories = [] # list of categories of lost/found items
-d = {} # dictionary of categories, subcategories and their counts
+d = { # dictionary of categories, subcategories and their counts
+	"name": "All Lost Property",
+	"children": []
+	} 
+
+dCh = d["children"] 
 
 # for each category of items, create a dictionary with the category as the keys and set the category's children nodes (item, count of item) as their values
 for children in root:
 	tag = children.attrib.get(children.tag)
 	if children.tag == "Category":
+		dCh.append({"category": tag, "children": []}) # create new sub-dictionary for every new category
+		for i in range(len(dCh)):
+			dChCh = dCh[i]["children"] # in which the children is made up of sub-sub-dictionaries including the item and the count
 		for child in children:
-			if not tag in d:
-				d[tag] = [child.attrib]
-			else:
-				d[tag].append((child.attrib))
+			dChCh.append({"item": child.attrib["SubCategory"], "count": child.attrib["count"]})
 
 # json-fy everything
-json_input = json.dumps(d, sort_keys = True, indent = 4, separators = (',', ": "))
+json_input = json.dumps(d, indent = 4, separators = (',', ": "))
 
 # print json-fied content to an actual json file
 json_output = open("lostandfound.json", "w")
