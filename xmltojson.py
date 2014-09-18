f3515ee52986c02e23542eb82a24647e1fadae74
Suyeon Son <suyeon.son@icloud.com>
2014-09-18 11:54:59 -0500

    oops json only had first children

--- xmltojson.py
+++ xmltojson.py
@@ -11,9 +11,13 @@ d = {} # dictionary of categories, subcategories and their counts
 
 # for each category of items, create a dictionary with the category as the keys and set the category's children nodes (item, count of item) as their values
 for children in root:
+	tag = children.attrib.get(children.tag)
 	if children.tag == "Category":
 		for child in children:
-			d[children.attrib.get(children.tag)] = [child.attrib]
+			if not tag in d:
+				d[tag] = [child.attrib]
+			else:
+				d[tag].append((child.attrib))
 
 # json-fy everything
 json_input = json.dumps(d, sort_keys = True, indent = 4, separators = (',', ": "))
